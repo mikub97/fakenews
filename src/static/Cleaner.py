0f99bbfc160a3771a72db6271270aaab70fba9a6
Michal <mikub97@gmail.com>
2019-05-28 17:43:36 +0200

    jeszcze lepsza struktura

--- src/static/Cleaner.py
+++ src/static/Cleaner.py
@@ -4,10 +4,6 @@ from textblob import TextBlob
 
 
 def clean_tweet(tweet):
-    '''
-    Utility function to clean tweet text by removing links, special characters
-    using simple regex statements.
-    '''
     return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])"
                            "|(\w+:\/\/\S+)",
                            " ",
