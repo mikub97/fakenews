ced4215fedbc5bfb5cf270f3455950f6075dfb54
Kuba <jakub.bielawski@wawasoft.com>
2019-05-30 00:26:26 +0200

    added probability,
    refactor return objects in Botchecker

--- src/twitter/BotChecket.py
+++ src/twitter/BotChecket.py
@@ -12,6 +12,17 @@ class BotChecker:
     def __init__(self):
         self.api = TwitterConnection().api
 
+    def parse_tweet(self, tweet, machineLearning):
+        parsed_tweet = {'username': tweet.user.screen_name,
+                        'retweets': tweet.retweet_count,
+                        'isBot': self.isBot(tweet),
+                        'isFakeBasedOnExternalUrl': self.is_fake_external_urls(tweet,
+                                                                               useMachineLearning=machineLearning),
+                        'full_text': tweet.full_text,
+                        'sentiment': self.getTweetSentiment(tweet),
+                        }
+        return parsed_tweet
+
     def getTweetSentiment(self, tweet):
         analysis = TextBlob(tweet.full_text)
         if analysis.sentiment.polarity > 0:
@@ -21,31 +32,70 @@ class BotChecker:
         else:
             return 'neutral'
 
-    #   Checking if tweet is fake based on frequency of posting the tweets
+    def get_tweet_sentiment(self, tweet):
+        parsed_tweet = self.parse_tweet(tweet)
+        return parsed_tweet['sentiment']
+
+    # Checking if tweet is fake based on frequency of posting the tweets
     def isBot(self, tweet):
         if tweet.retweet_count > 0:
-            return False
+            result = {
+                'fake': False,
+                'probability': 1,
+                'description': 'Tweet był retweetowany - to nie jest bot'
+            }
+            return result
         else:
             user_name = tweet.user.screen_name
             last_tweets = self.api.user_timeline(screen_name=user_name, count=10, tweet_mode='extended',
                                                  include_entities=True)
             if len(last_tweets) < 10:
-                return True
+                result = {
+                    'fake': True,
+                    'probability': 0.7,
+                    'description': 'User nie opublikowal 10 tweetow - uznajemy za bota'
+                }
+                return result
             else:
+                result = {
+                    'fake': False,
+                    'probability': 'default value',
+                    'description': 'default value'
+                }
                 for idx, tweet in enumerate(last_tweets):
                     if idx > 0:
                         current_tweet_date = last_tweets[idx].created_at
                         tweet_date = last_tweets[idx - 1].created_at
                         two_days = datetime.timedelta(days=2)
-                        if (tweet_date - current_tweet_date) > two_days:
-                            return True
-            return False
+                        three_days = datetime.timedelta(days=3)
+                        five_days = datetime.timedelta(days=5)
+                        if (tweet_date - current_tweet_date) < two_days:
+                            result['fake'] = False
+                            result['probability'] = 1
+                            result['description'] = 'User publikuje z czestotliwoscia wieksza niz dwa dni - to nie ' \
+                                                    'jest bot '
+                        elif three_days < (tweet_date - current_tweet_date) < five_days:
+                            result['fake'] = True
+                            result['probability'] = 0.7
+                            result['description'] = 'User nie publikowal nic przez 3,4 lub 5 dni - uzanje za bota z ' \
+                                                    'prawd.=0.6 '
+                        else:
+                            result['fake'] = True
+                            result['probability'] = 0.9
+                            result['description'] = 'User ma odstep miedzy tweetami wiekszy niz 5 dni - uznaje ' \
+                                                    'za bota z prawd.=0.8 '
+            return result
 
     # Checking if tweet is fake based on external urls provided in a tweet
     def is_fake_external_urls(self, tweet, useMachineLearning):
         urls = tweet.entities['urls']
         if len(urls) <= 0:
-            return False
+            result = {
+                'fake': 'default value',
+                'probability': 'default value',
+                'description': 'Brak URLi w tweecie'
+            }
+            return result
         for url in urls:
             full_url = url['expanded_url']
             if not useMachineLearning:
@@ -59,62 +109,64 @@ class BotChecker:
                     response = requests.get(full_url, headers=headers)
                     http_code = response.status_code
                     if (http_code / 100) >= 4:
-                        return True
+                        result = {
+                            'fake': True,
+                            'probability': 1,
+                            'description': 'Wylaczono machine learning'
+                        }
+                        return result
                 except:
-                    print('Site does not let us in, conisdering tweet as fake')
-                    return True
+                    print('Site does not let us in, considering tweet as fake')
+                    result = {
+                        'fake': True,
+                        'probability': 1,
+                        'descripiton': 'Wylaczono machine learning'
+                    }
+                    return result
 
-                return False
+                result = {
+                    'fake': False,
+                    'probability': 0.7,
+                    'description': 'Wylaczono machine learning, Url nie rzuca bledem ale nieznany jest content url, zwracam prawdo.=0.7'
+                }
+                return result
             else:
                 data_machine_learner = UrlMachineLearner()
-                return data_machine_learner.is_url_malicious(full_url)
+                url_malicious = data_machine_learner.is_url_malicious(full_url)
 
-    def parse_tweet(self, tweet, machineLearning):
-        parsed_tweet = {'username': tweet.user.screen_name,
-                        'retweets': tweet.retweet_count,
-                        'isBot': self.isBot(tweet),
-                        'isFakeBasedOnExternalUrl': self.is_fake_external_urls(tweet,
-                                                                               useMachineLearning=machineLearning),
-                        'full_text': tweet.full_text,
-                        'sentiment': self.getTweetSentiment(tweet),
-                        }
-        return parsed_tweet
+                if url_malicious['malicious']:
+                    result = {
+                        'fake': True,
+                        'probability': url_malicious['score'],
+                        'description': 'wlaczono machine learning'
+                    }
+                else:
+                    result = {
+                        'fake': False,
+                        'probability': url_malicious['score'],
+                        'description': 'wlaczono machine learning'
+                    }
 
+                return result
+
+    # method to be used by other modules
     def is_fake_based_on_user(self, tweet):
-        parsed_tweet = self.parse_tweet(tweet, True)
-        return parsed_tweet['isBot']
+        return self.isBot(tweet)
 
-    def is_fake_based_on_external_urls(self, tweet):
-        parsed_tweet = self.parse_tweet(tweet, True)
-        return parsed_tweet['isFakeBasedOnExternalUrl']
-
-    def get_tweet_sentiment(self, tweet):
-        parsed_tweet = self.parse_tweet(tweet)
-        return parsed_tweet['sentiment']
-
-    # Determine is tweet is fake based on frequency of posted tweets by user and on external urls in tweets if
-    # machineLearning == true than program will check the urls in tweets based on machine learning - not very
-    # accurate
-    # if not machineLearning porgram will check the HTTP status to the external urls - accurate but not
-    # checking if url is malicious
-    def is_fake(self, tweet, isMachineLearnignOn):
-        if isMachineLearnignOn:
-            parsed_tweet = self.parse_tweet(tweet, True)
-            return parsed_tweet['isBot'] or parsed_tweet['isFakeBasedOnExternalUrl']
-        else:
-            parsed_tweet = self.parse_tweet(tweet, False)
-            return parsed_tweet['isBot'] or parsed_tweet['isFakeBasedOnExternalUrl']
+    # method to be used by other modules
+    def is_fake_based_on_external_urls(self, tweet, isMachineLearning):
+        return self.is_fake_external_urls(tweet, isMachineLearning)
 
 
-#Sample invoke
+# Sample invoke
 def main():
     obj = BotChecker()
-    tweets = obj.api.search(q="Donald Trump", tweet_mode='extended', count=100, include_entities=True)
+    tweets = obj.api.search(q="Donald Trump", tweet_mode='extended', count=20, include_entities=True)
     for tweet in tweets:
         print('is fake based on user: ', obj.is_fake_based_on_user(tweet))
-        print('is fake based on external urls: ', obj.is_fake_external_urls(tweet))
+        print('is fake based on external urls: ', obj.is_fake_external_urls(tweet, True))
 
 
-#Sample invoke
+# Sample invoke
 if __name__ == '__main__':
     main()
