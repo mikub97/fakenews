c6f1d040217d99ab428d039aa358eddc0ea3a0b7
wojciechmusiato <wojciech.musiato@gmail.com>
2019-05-30 22:00:28 +0100

    poprawki

--- src/machinelearning/Predictor.py
+++ src/machinelearning/Predictor.py
@@ -1,13 +1,15 @@
 import joblib
-import DataLoader
+from src.machinelearning import DataLoader
+
 
 
 class Predictor:
 
     def __init__(self):
-        self.load_model = joblib.load('prediction_model.sav')
+        self.load_model = joblib.load('./machinelearning/prediction_model.sav')
+
     def predict(self, var):
-        load_model = joblib.load('prediction_model.sav')
+        load_model = joblib.load('./machinelearning/prediction_model.sav')
 
         prediction = load_model.predict(DataLoader.stem(var))
         prob = load_model.predict_proba(DataLoader.stem(var))
