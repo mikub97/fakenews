280fbbb6cd583b2d74fb13539028c1658d7281d7
Bartosz Wnorowski <wnbartek@wp.pl>
2019-05-31 00:39:01 +0200

    id v2

--- src/machinelearning/Predictor.py
+++ src/machinelearning/Predictor.py
@@ -12,15 +12,16 @@ class Predictor:
         fetcher = Fetcher()
         load_model = joblib.load('./machinelearning/prediction_model.sav')
         tweet = fetcher.get_tweet(id)
-        prediction = load_model.predict(tweet['full_text'])
-        prob = load_model.predict_proba(tweet['full_text'])
+        input = DataLoader.stem([tweet['full_text']])
+        prediction = load_model.predict(input)
+        prob = load_model.predict_proba(input)
         answ = {}
         if prediction[0] == 'True':
             answ['description'] = 'ML model DO NOT classify this as a Fake News.'
         else:
             answ['description'] = 'ML model classify this as a Fake News.'
         answ['probability'] = prob[0][0]
-	
+
         return (answ)
 
 
